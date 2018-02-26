fc8167396f4f81554e9cae1937b2973a7a045d52
AlexAgnoII <3alex23@gmail.com>
2018-02-26 19:12:33 +0800

    level 3 done!

--- script/game.js
+++ script/game.js
@@ -49,7 +49,7 @@ function setup() {
     level._complete = true; //initially true  to prevent movement
     
     //testing
-    level._currentLevel = 1;
+    level._currentLevel = 2;
     
 
     state = title;
